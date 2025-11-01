1f6a6a584672807292f3ce56110e2f3bd1f4d96c
Harshal Bagade <harshalgb824@gmail.com>
2025-11-01 20:53:30 +0530

    Added New files

---
+++ ArraylistQues.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class ArraylistQues {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        list.get(1);
+        list.set(1,3);
+
+        
+
+        
+        
+    }
+}
---
+++ SearchInSortedArray.java
@@ -0,0 +1,31 @@
+public class SearchInSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int start_idx = -1;
+        int last_idx = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (start_idx == -1) {
+                    start_idx = i; 
+                }
+                last_idx = i; 
+            }
+        }
+        
+        // Store indices in result_arr
+        int[] result_arr = new int[]{start_idx, last_idx};
+        
+        return result_arr; // Return the result
+    }
+    public static void main(String[] args) {
+        int[] nums={5,1,4,6,6,7};
+        int target=6;
+        SearchInSortedArray sol=new SearchInSortedArray();
+        int result[]=sol.searchRange(nums, target);
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+
+    }
+
+}
